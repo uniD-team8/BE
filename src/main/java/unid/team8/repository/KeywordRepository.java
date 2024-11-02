b8a475679dc7810bcf54b97faca48b38506d6fe2
GY102912 <qaz102912@gmail.com>
2024-11-03 05:45:46 +0900

    feat: finish top2 keywords

--- src/main/java/unid/team8/repository/KeywordRepository.java
+++ src/main/java/unid/team8/repository/KeywordRepository.java
@@ -1,8 +1,10 @@
 package unid.team8.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import unid.team8.entity.Keyword;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     Keyword findByUser_Id(Long userId);
+    Optional<Keyword> findByUserId(Long userId);
 }
