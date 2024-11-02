0e3c7fb1dbbe5228ffdcf5f0e8cd425f908c93c2
GY102912 <qaz102912@gmail.com>
2024-11-03 00:28:37 +0900

    feat: initial setting

---
+++ src/main/java/unid/team8/Team8Application.java
@@ -0,0 +1,13 @@
+package unid.team8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team8Application.class, args);
+	}
+
+}
