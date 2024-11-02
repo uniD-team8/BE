cc00bfd1b8435f1b437c1c1e15434dfb20729ccd
GY102912 <qaz102912@gmail.com>
2024-11-03 00:33:10 +0900

    feat: modified initial setting

---
+++ src/main/java/unid/team8/entity/User.java
@@ -0,0 +1,20 @@
+package unid.team8.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name="user_id")
+  Long id;
+}
