2f47c14a4fabf64285ad4008da3db49f202ca636
donghun1214 <acekdhh@g.skku.edu>
2024-11-03 01:53:46 +0900

    add medicinemission, user entity

--- src/main/java/unid/team8/entity/User.java
+++ src/main/java/unid/team8/entity/User.java
@@ -1,20 +1,39 @@
 package unid.team8.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
 
 @Entity
+@Table(name = "User")
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer userId;
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name="user_id")
-  Long id;
+    private String photo;
+
+    private String name;
+
+    private Integer point;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private MedicineMission medicineMission;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "is_Parent", nullable = false)
+    private Boolean isParent;
+
+    @Column(name = "family_code", nullable = false)
+    private String familyCode;
+
+    @Column(name = "gift_id", nullable = false)
+    private Long giftId;
 }
